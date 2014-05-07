9b41b368ec23aa816a2ab45c1bfbec07b6f1b376
saasbook <saasbook@saasbook.(none)>
2014-05-07 13:14:52 -0300

    Rspec trial again

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-=begin
+
 
 Rspec tests not nessassary because models are not used
 This is because nothing is saved to the database, the results are just rendered
@@ -8,25 +8,22 @@ This is because nothing is saved to the database, the results are just rendered
 describe HomeController do
 	describe 'searching for charities' do
 		before :each do
-			@results = [{:name => 'fake', :uri =>'fake.com'}]
-			#hard code zip code too
-			set(:@category, 'scouts')
+			@results = [mock('charity1'), mock('charity2')]
 		end
 
-		it 'should call the helper method that performs the search'
+		it 'should call the method that performs the search by location and keywords'
 			#based on model that we will have
-			assigns(:@category).should == 'scouts'
-			@top_charities.should == @results
+			Charity.should_recieve(:results).with('Animals')
 
 		it 'should select the Search Results template for rendering'
 			#stub method may or may not be called
-			Home.stub(:charities_grabber).and_return(results)
-			post :search_charities, {:search_terms => 'Computers'}
-			response.should render_template('search_charities')
+			Home.stub(:results).and_return(@results)
+			post :results, {:search_terms => 'Animals'}
+			response.should render_template('results')
 
 		it 'should make the charity search results available to that template'
-			Home.stub(:find_in_DB).and_return(results)
+			Home.stub(:results).and_return(results)
 			assings(@top_charities).should == @results
-=end
+
 	end
 end
