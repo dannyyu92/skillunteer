126548c4b83b8fba1f76bb166c0a41c3ef2e9cd1
saasbook <saasbook@saasbook.(none)>
2014-05-08 11:33:22 -0700

    rspec updates

--- spec/controllers/charities_controller_spec.rb
+++ spec/controllers/charities_controller_spec.rb
@@ -2,12 +2,34 @@ require 'spec_helper'
 
 describe CharitiesController do
 	before(:each) do
-		@return = mock('charity1')
+		@return = mock('charity1', :id => 0)
 	end
+
 	describe 'new' do
 		it 'Adds a charity to the database' do
 			Charity.should_receive(:new).and_return(@return)
 			get :new, :charity => {:category => "test"}
 		end
 	end
+
+	describe 'destroy' do
+		it 'Removes a charity from the database' do
+			Charity.should_receive(:destroy)
+			delete :destroy, :id => 0
+		end
+
+		it 'Redirects to charities path' do
+			Charity.stub(:find).with(:id => 1).and_return(@return)
+			response.should redirect_to charities_path
+		end
+	end
+
+	describe 'update' do
+		it 'Updates charity name' do
+			Charity.should_receive(:update).and_return(@return)
+			put :update, :id => 0
+		end
+	end
+
+
 end
